2f37575ea0663e47704a76f0a0b87edd1e0547f3
divby0exc <seddigh.daniel@gmail.com>
2023-09-18 15:38:15 +0200

    autowired respectively repository in the service impl classes

--- src/main/java/com/divby0exc/testingandci/service/AccountService.java
+++ src/main/java/com/divby0exc/testingandci/service/AccountService.java
@@ -1,7 +1,11 @@
 package com.divby0exc.testingandci.service;
 
+import com.divby0exc.testingandci.repository.IAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
+    @Autowired
+    IAccountRepository repository;
 }
--- src/main/java/com/divby0exc/testingandci/service/ActiveBookingService.java
+++ src/main/java/com/divby0exc/testingandci/service/ActiveBookingService.java
@@ -1,7 +1,11 @@
 package com.divby0exc.testingandci.service;
 
+import com.divby0exc.testingandci.repository.IActiveBookingsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ActiveBookingService {
+    @Autowired
+    IActiveBookingsRepository repository;
 }
