07477ac846109093aeda33cf0fa4470d027e03a9
divby0exc <seddigh.daniel@gmail.com>
2023-09-20 11:26:20 +0200

    combined the three classes to AccountTest class. bcz they already focus on one class so why not

--- src/test/java/com/divby0exc/testingandci/model/AccountTest.java
+++ src/test/java/com/divby0exc/testingandci/model/AccountTest.java
@@ -1,12 +1,24 @@
 package com.divby0exc.testingandci.model;
 
+import com.divby0exc.testingandci.service.AccountService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AccountTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+    private AccountService accountService;
     @Test
     public void testGettersAndSetters() {
         // Create a mock Account object
@@ -25,6 +37,53 @@ public class AccountTest {
         assertEquals("daniel@gmail.com", account.getContactInfo());
         assertEquals(123456, account.getPaymentInfo());
         assertEquals("USER", account.getAccountType());
+    }
+
+    @Test
+    public void testCreateAndRetrieveAccount() {
+        // Define the base URL for account creation endpoint
+        String baseUrl = "http://localhost:" + 8080 + "/create_account";
+
+        // Create an account object then
+        // Make a HTTP req to save the account thro the endpoint
+        Account account = new Account();
+        account.setUsername("Daniel");
+        account.setContactInfo("daniel@gmail.com");
+        account.setPaymentInfo(123456);
+        account.setAccountType("USER");
+
+        Account createdAccount = restTemplate.postForObject(baseUrl, account, Account.class);
+
+        // Verify the response and account creation
+        assertNotNull(createdAccount.getId());
+        assertEquals("Daniel", createdAccount.getUsername());
+        assertEquals("daniel@gmail.com", createdAccount.getContactInfo());
+        assertEquals(123456, createdAccount.getPaymentInfo());
+        assertEquals("USER", createdAccount.getAccountType());
+    }
+
+    @Test
+    public void testSaveAccount() {
+        // Create an instance of Account
+        Account account = new Account();
+        account.setUsername("Daniel");
+        account.setContactInfo("daniel@gmail.com");
+        account.setPaymentInfo(123456);
+        account.setAccountType("USER");
+
+        // Save the account to the database
+        Account savedAccount = accountService.saveAccount(account);
+
+        // Retrieve the account from the database
+        Account retrievedAccount = accountService.fetchedAccount(savedAccount.getId()).orElse(null);
+
+        // Perform assertions to verify the save and retrieve operations
+        assertNotNull(retrievedAccount);
+        assertEquals("Daniel", retrievedAccount.getUsername());
+        assertEquals("daniel@gmail.com", retrievedAccount.getContactInfo());
+        assertEquals(123456, retrievedAccount.getPaymentInfo());
+        assertEquals("USER", retrievedAccount.getAccountType());
+
     }
 
 }
