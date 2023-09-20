b7245f4341e39f027ddcce9ac5501d9a1b07ebc4
divby0exc <seddigh.daniel@gmail.com>
2023-09-20 11:17:36 +0200

    Rest template didn't want me to delete springboottest annotation and not change random_port to defined_port. The port is defined so it seems odd to have it but yea. Anywho, defined endtoendtest class and started with one test, gonna test all fields to be sure

--- src/main/java/com/divby0exc/testingandci/controller/AccountController.java
+++ src/main/java/com/divby0exc/testingandci/controller/AccountController.java
@@ -3,7 +3,9 @@ package com.divby0exc.testingandci.controller;
 import com.divby0exc.testingandci.model.Account;
 import com.divby0exc.testingandci.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,13 +14,7 @@ public class AccountController {
     private AccountService accountService;
     // Save operation
     @PostMapping("/create_account")
-    public Account saveAccount() {
-        Account account = Account.builder()
-                .username("Josef")
-                .contactInfo("112")
-                .accountType("ADMIN")
-                .paymentInfo(112)
-                .build();
+    public Account saveAccount(@RequestBody Account account) {
         return accountService.saveAccount(account);
     }
 }
