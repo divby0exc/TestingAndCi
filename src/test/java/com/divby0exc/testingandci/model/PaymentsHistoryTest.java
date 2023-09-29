f6d1ea6367ca0301fc6091c1e293d20110db6292
divby0exc <seddigh.daniel@gmail.com>
2023-09-29 12:07:04 +0200

    added mocked repo-service and real service

--- src/test/java/com/divby0exc/testingandci/model/PaymentsHistoryTest.java
+++ src/test/java/com/divby0exc/testingandci/model/PaymentsHistoryTest.java
@@ -1,8 +1,13 @@
 package com.divby0exc.testingandci.model;
 
 import com.divby0exc.testingandci.TestingAndCiApplication;
+import com.divby0exc.testingandci.repository.IPaymentHistoryRepository;
+import com.divby0exc.testingandci.service.PaymentHistoryService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -12,5 +17,14 @@ import static org.junit.jupiter.api.Assertions.*;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @ExtendWith(MockitoExtension.class)
 class PaymentsHistoryTest {
+
+    @Mock
+    IPaymentHistoryRepository repo;
+
+    @InjectMocks
+    PaymentHistoryService paymentHistoryServiceWithMock;
+
+    @Autowired
+    PaymentHistoryService paymentHistoryService;
 
 }
