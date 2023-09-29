b46b71465f87322871ba85ba919719f2b69158fe
divby0exc <seddigh.daniel@gmail.com>
2023-09-29 12:05:47 +0200

    created a test class for paymenthistory and added relevant annotations for the class

---
+++ src/test/java/com/divby0exc/testingandci/model/PaymentsHistoryTest.java
@@ -0,0 +1,16 @@
+package com.divby0exc.testingandci.model;
+
+import com.divby0exc.testingandci.TestingAndCiApplication;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = {TestingAndCiApplication.class})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ExtendWith(MockitoExtension.class)
+class PaymentsHistoryTest {
+
+}
