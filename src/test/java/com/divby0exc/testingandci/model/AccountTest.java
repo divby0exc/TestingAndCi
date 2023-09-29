c90791b2e95cf0f5250b81ee333d7e714df0b72a
divby0exc <seddigh.daniel@gmail.com>
2023-09-29 09:54:31 +0200

    added username edge cases

--- src/test/java/com/divby0exc/testingandci/model/AccountTest.java
+++ src/test/java/com/divby0exc/testingandci/model/AccountTest.java
@@ -2,21 +2,18 @@ package com.divby0exc.testingandci.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class AccountTest {
 
     @Test
-    public void testingEdgeCasesOnAccountCreation() {
-//        Account account1 = new Account();
-//        Account account2 = new Account();
-//        account1.setAccountType();
+    public void testingIfUsernameIsNotNull() {
+
     }
 
     @Test
-    public void integrationTest() {
+    public void testingIfUsernameIsNotEmpty() {
 
     }
+
 
 
 
