c7ac2ca1b1cd01368faaeaab19e62c3ff5d0c239
divby0exc <seddigh.daniel@gmail.com>
2023-09-29 12:22:42 +0200

    added some description to the test method

--- src/test/java/com/divby0exc/testingandci/model/PaymentsHistoryTest.java
+++ src/test/java/com/divby0exc/testingandci/model/PaymentsHistoryTest.java
@@ -29,7 +29,7 @@ class PaymentsHistoryTest {
     PaymentHistoryService paymentHistoryService;
 
     @Test
-    public void testThatInvalidPaymentIdIsNotThrown() {
+    public void testThatInvalidPaymentIdIsNotThrownWhenYouFetchPaymentList() {
 
     }
 
