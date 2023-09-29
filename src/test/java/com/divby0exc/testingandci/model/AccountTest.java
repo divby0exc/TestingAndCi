bfed991c7e21e977ce16b43b6bb8f55265d2ed2f
divby0exc <seddigh.daniel@gmail.com>
2023-09-29 10:01:23 +0200

    corrected a typo in phoneNumber 11 digits method

--- src/test/java/com/divby0exc/testingandci/model/AccountTest.java
+++ src/test/java/com/divby0exc/testingandci/model/AccountTest.java
@@ -55,7 +55,7 @@ class AccountTest {
     }
 
     @Test
-    public void testingfIfPhoneNumberThatStartsWith_Plus46_Is_11_Digits() {
+    public void testingIfPhoneNumberThatStartsWith_Plus46_Is_11_Digits() {
 
     }
 
