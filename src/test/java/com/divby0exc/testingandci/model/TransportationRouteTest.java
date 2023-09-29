5c76a0d40d1bf0eb382b0b6d07695b9484fd38f8
divby0exc <seddigh.daniel@gmail.com>
2023-09-29 12:52:40 +0200

    added mocked service and real service

--- src/test/java/com/divby0exc/testingandci/model/TransportationRouteTest.java
+++ src/test/java/com/divby0exc/testingandci/model/TransportationRouteTest.java
@@ -1,8 +1,13 @@
 package com.divby0exc.testingandci.model;
 
 import com.divby0exc.testingandci.TestingAndCiApplication;
+import com.divby0exc.testingandci.repository.ITransportationRouteRepository;
+import com.divby0exc.testingandci.service.TransportationRouteService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -12,5 +17,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @ExtendWith(MockitoExtension.class)
 class TransportationRouteTest {
+    @Mock
+    ITransportationRouteRepository repo;
+
+    @InjectMocks
+    TransportationRouteService routeServiceWithMockedRepo;
+
+    @Autowired
+    TransportationRouteService routeService;
 
 }
