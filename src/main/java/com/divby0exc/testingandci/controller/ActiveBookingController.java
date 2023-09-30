3b41fc003f902fa49ddd91d607696199a87e2aa8
divby0exc <seddigh.daniel@gmail.com>
2023-10-01 01:33:21 +0200

    changed all return values in active controller to responseentity

--- src/main/java/com/divby0exc/testingandci/controller/ActiveBookingController.java
+++ src/main/java/com/divby0exc/testingandci/controller/ActiveBookingController.java
@@ -4,6 +4,8 @@ import com.divby0exc.testingandci.handlerexception.InvalidBookingIdException;
 import com.divby0exc.testingandci.model.ActiveBookings;
 import com.divby0exc.testingandci.service.ActiveBookingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,22 +20,22 @@ public class ActiveBookingController {
 
     //    POST
     @PostMapping("create_booking")
-    public ActiveBookings saveBooking(@RequestBody ActiveBookings activeBookings) {
+    public ResponseEntity<ActiveBookings> saveBooking(@RequestBody ActiveBookings activeBookings) {
         return activeBookingService.createNewBooking(activeBookings);
     }
     //    GET
     @GetMapping("get_booking/{accountId}")
-    public Optional<ActiveBookings> fetchActiveBooking(@PathVariable Long accountId) throws InvalidBookingIdException {
+    public ResponseEntity<Optional<ActiveBookings>> fetchActiveBooking(@PathVariable Long accountId) throws InvalidBookingIdException {
         return activeBookingService.fetchOneBooking(accountId);
     }
     //    GET
     @GetMapping("get_booking_list/{accountId}")
-    public List<ActiveBookings> fetchActiveBookingList(@PathVariable Long accountId) throws InvalidBookingIdException {
+    public ResponseEntity<List<ActiveBookings>> fetchActiveBookingList(@PathVariable Long accountId) throws InvalidBookingIdException {
         return activeBookingService.fetchActiveBookingList(accountId);
     }
     //    Delete
     @DeleteMapping("delete_booking/{accountId}")
-    public void deleteBooking(@PathVariable Long accountId) throws InvalidBookingIdException {
+    public ResponseEntity<HttpStatus> deleteBooking(@PathVariable Long accountId) throws InvalidBookingIdException {
         activeBookingService.deleteBooking(accountId);
     }
 }
