1f827d31f3f7ac0a332aeef0ff6e67e5a3fc7943
divby0exc <seddigh.daniel@gmail.com>
2023-09-30 18:59:12 +0200

    created buyATicket test class

---
+++ src/test/java/com/divby0exc/testingandci/controller/TicketBookingControllerTest.java
@@ -0,0 +1,12 @@
+package com.divby0exc.testingandci.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketBookingControllerTest {
+
+    @Test
+    void buyATicket() {
+    }
+}
