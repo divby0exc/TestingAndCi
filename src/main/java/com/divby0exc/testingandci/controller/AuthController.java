1a30e3b1143cd734a82b6a946509b72c05970e7d
divby0exc <seddigh.daniel@gmail.com>
2023-10-02 11:59:21 +0200

    added dependency for open-api. changed param in authcontroller to only id and username. added path in app.prop to api-doc

--- src/main/java/com/divby0exc/testingandci/controller/AuthController.java
+++ src/main/java/com/divby0exc/testingandci/controller/AuthController.java
@@ -4,19 +4,16 @@ import com.divby0exc.testingandci.model.Account;
 import com.divby0exc.testingandci.util.JWTUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth/")
 public class AuthController {
 
     @PostMapping("login")
-    public ResponseEntity<String> signIn(@RequestBody Account authDetails) {
+    public ResponseEntity<String> signIn(@RequestParam String username, @RequestParam String id) {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(JWTUtil.createJWT(authDetails.getId().toString(), authDetails.getUsername()));
+                .body(JWTUtil.createJWT(id, username));
     }
 }
